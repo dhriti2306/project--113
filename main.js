1cd8753860d420fd8efb9f8710c85b8a03a1f530
dhriti2306 <82763591+dhriti2306@users.noreply.github.com>
2021-06-10 18:12:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(640 , 480);
+    canvas.position(110 , 250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+    
+
+    function draw(){
+        image(video,70,40,500,400);
+        
+        fill(255,0,0);
+        stroke(255,0,0);
+        circle(20,20,100);
+        
+        circle(20,460,100);
+        circle(620,20,100);
+        circle(620,460,100)
+        
+        fill(0,255,0);
+        stroke(0,255,0);
+        rect(75,0,490,20);
+        
+        rect(75,460,490,20);
+        }
+
+        function take_snapshot(){
+            save('Student.png');
+        }
